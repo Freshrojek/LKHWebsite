236be9537c749d97d6f2d42d0e2ed3ec74b9c014
Jack Boyer <jnboyer1@gmail.com>
2021-04-14 11:12:09 +0100

    Update settings.py

--- src/LKHWebsite/settings.py
+++ src/LKHWebsite/settings.py
@@ -26,8 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = config('DEBUG', cast=bool)
-DEBUG = os.environ.get("DEBUG")
-SECRET_KEY = os.environ.get("SECRET_KEY")
+# DEBUG = os.environ.get("DEBUG")
+# SECRET_KEY = os.environ.get("SECRET_KEY")
+DEBUG = True
+SECRET_KEY = ''
 
 ALLOWED_HOSTS = []
 
