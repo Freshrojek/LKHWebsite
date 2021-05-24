e29629a236aef27d2f8797c6277ddad335629a2f
Jack Boyer <jnboyer1@gmail.com>
2021-05-24 22:18:19 +0100

    Minor fixes/imporvements
    Fixed broken link for the navbar and made sure all templates where using the same styling

--- src/LKHWebsite/urls.py
+++ src/LKHWebsite/urls.py
@@ -24,7 +24,7 @@ from django.conf import settings
 urlpatterns = [
 
     path('home/', home_view, name='home'),
-    path("contact/", contact, name="contact"),
+    path("contact/", contact_view, name="contact"),
     path('about/', about_view, name="about"),
     path('product/', product_detail_view),
     path('create/', product_create_view),
