d996080d2c595925bc2cb893ee0c5b7fbbf9dd59
Jack Boyer <jnboyer1@gmail.com>
2022-09-16 10:58:55 +0100

    commiting unpushed changes

--- LKHWebsite/articles/models.py
+++ LKHWebsite/articles/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from datetime import date
 
 # Create your models here.
 class Article(models.Model):
@@ -6,7 +7,9 @@ class Article(models.Model):
     name = models.CharField(max_length=64)
     author = models.ForeignKey('articles.Author', on_delete=models.CASCADE)
     content = models.TextField()
+    overview = models.TextField()
     slug = models.CharField(default='', max_length=64)
+    date = models.DateField("Date", auto_now_add=True)
 
     def __str__(self):
         return '%s' % self.name
